7c50afe88398d67379da0d15a371d24ead38e65b
Andrew Huynh <andrew.huynh@bti360.com>
2017-12-21 11:34:51 -0500

    fixed sentiment analysis and made multiprocessed approach

--- sodium.py
+++ sodium.py
@@ -3,14 +3,18 @@ from nltk.tokenize import sent_tokenize
 import praw
 import sqlite3
 import multiprocessing
+import threading
 import datetime
 import numpy
 
 import auths
 
-# First time setup requires vader_lexicon. Subsequent runs do not require these two lines of code
+# ================================================================================
+# First time setup. Subsequent runs do not require this code block
 # import nltk
 # nltk.download('vader_lexicon')
+# nltk.download('punkt')
+# ================================================================================
 
 db = sqlite3.connect('sodium.db')
 db.row_factory = sqlite3.Row
@@ -25,8 +29,6 @@ analyzer = SentimentIntensityAnalyzer()
 reddit = praw.Reddit(client_id=auths.client,
                      client_secret=auths.secret,
                      user_agent='saxypandabear:reddit-sentiment-analyzer:v0.1')
-
-pool = multiprocessing.Pool(processes=10)
 
 
 # takes a list and returns a concatenation of the items.
@@ -43,12 +45,16 @@ def get_subreddits():
 def get_submission_sentiment(submission):
     timestamp = datetime.datetime.fromtimestamp(submission.created)
     text = submission.selftext
-    sentences = sent_tokenize(text)
+
+    sentences = sent_tokenize(text)               # tokenize the text within the post
+    sentences += sent_tokenize(submission.title)  # include the title of the post
+
     # for each sentence, calculate the compound sentiment
-    sentence_sentiments = pool.map_async(get_sentence_sentiment, sentences)
+    # should not have empty list of sentiments because there should be at least a non-empty title
+    sentiments = [get_sentence_sentiment(sentence) for sentence in sentences]
 
     # return the id, the datetime of submission, and the average compound sentiment
-    return submission.id, timestamp, numpy.average(sentence_sentiments.get())
+    return submission.id, numpy.average(sentiments), timestamp
 
 
 # uses the NLTK Vader sentiment analyzer to calculate the compound sentiment of a given sentence
@@ -56,7 +62,26 @@ def get_sentence_sentiment(sentence):
     return analyzer.polarity_scores(sentence)['compound']
 
 
-submissions = reddit.subreddit(get_subreddits()).hot(limit=100)
-results = pool.map(get_submission_sentiment, submissions)
+# define the function that will be run indefinitely in the script
+def analyze_sentiments():
+    threading.Timer(86400, analyze_sentiments).start()  # run this once every day
+    pool = multiprocessing.Pool(processes=5)
+    submissions = reddit.subreddit(get_subreddits()).hot(limit=50)  # first 2 pages
+    results = pool.map(get_submission_sentiment, submissions)
+    pool.close()  # close the processes after they are done being used
+    # print(results)
 
-print(results)
+    # after getting results, write to DB
+    for values in results:
+        # print(values)
+        db.execute("INSERT OR IGNORE INTO sodium VALUES (?,?,?)", values)
+    db.commit()  # after writing all of the items into the database, commit to the disk file
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            analyze_sentiments()
+    except:
+        db.close()  # try to gracefully close the database on KeyboardInterrupt
+        exit(0)
